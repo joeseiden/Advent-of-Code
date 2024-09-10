190962d7847ae3aad322642721e41bdf983107e8
Joe Seiden <joe.seiden214@gmail.com>
2024-09-10 13:03:54 -0700

    Refactor parts 1 and 2 to be more efficient and DRY

--- 2023_day2_cube_conundrum/main.js
+++ 2023_day2_cube_conundrum/main.js
@@ -3,25 +3,23 @@ const input = module.require('./input.js');
 const inputArray = input.split('\n');
 
 const calculatePossibleGames = (gamesArray) => {
-    const maxPossibleCubes = {
-        red: 12,
-        green: 13,
-        blue: 14
-    };
+    const [possibleRed, possibleGreen, possibleBlue] = [12, 13, 14];
     
     let possibleGamesSum = 0;
     
-    const redRegex = /(\d+) red/g;
-    const blueRegex = /(\d+) blue/g;
-    const greenRegex = /(\d+) green/g;
-    
+    const findMaxOfColor = (game, color) => {
+        const re = new RegExp(`(\\d+) ${color}`, 'g');
+        const max = [...game.matchAll(re)]
+                        .map(num => parseInt(num))
+                        .reduce((a,b) => Math.max(a, b), -Infinity);
+        return max;
+    }
+
+    findMaxOfColor('red');
     gamesArray.forEach((game, i) => {
-        const maxRed = [...game.matchAll(redRegex)].map(el => parseInt(el)).reduce((a, b) => Math.max(a,b), -Infinity);
-        const maxBlue = [...game.matchAll(blueRegex)].map(el => parseInt(el)).reduce((a, b) => Math.max(a,b), -Infinity);
-        const maxGreen = [...game.matchAll(greenRegex)].map(el => parseInt(el)).reduce((a, b) => Math.max(a,b), -Infinity);
-        console.log(maxRed, maxBlue, maxGreen);
-        
-        if(maxRed <= maxPossibleCubes.red && maxBlue <= maxPossibleCubes.blue && maxGreen <= maxPossibleCubes.green
+        [maxRed, maxBlue, maxGreen] = ['red', 'blue', 'green'].map(color => findMaxOfColor(game, color));
+ 
+        if(maxRed <= possibleRed && maxBlue <= possibleBlue && maxGreen <= possibleGreen
         ) {
             possibleGamesSum += (i + 1)
         }
--- 2023_day2_cube_conundrum/main2.js
+++ 2023_day2_cube_conundrum/main2.js
@@ -5,16 +5,17 @@ const inputArray = input.split('\n');
 const calculateTotalGamesPower = (gamesArray) => {    
     let totalGamePowerSum = 0;
     
-    const redRegex = /(\d+) red/g;
-    const blueRegex = /(\d+) blue/g;
-    const greenRegex = /(\d+) green/g;
-    
-    gamesArray.forEach((game) => {
-        const maxRed = [...game.matchAll(redRegex)].map(el => parseInt(el)).reduce((a, b) => Math.max(a,b), -Infinity);
-        const maxBlue = [...game.matchAll(blueRegex)].map(el => parseInt(el)).reduce((a, b) => Math.max(a,b), -Infinity);
-        const maxGreen = [...game.matchAll(greenRegex)].map(el => parseInt(el)).reduce((a, b) => Math.max(a,b), -Infinity);
+    const findMaxOfColor = (game, color) => {
+        const re = new RegExp(`(\\d+) ${color}`, 'g');
+        const max = [...game.matchAll(re)]
+                        .map(num => parseInt(num))
+                        .reduce((a,b) => Math.max(a, b), -Infinity);
+        return max;
+    }
 
-        const power = maxRed * maxBlue * maxGreen;
+    gamesArray.forEach((game) => {
+        const maxColorCounts = ['red', 'blue', 'green'].map(color => findMaxOfColor(game, color));
+        const power = maxColorCounts.reduce((a,b) => a * b);
 
         totalGamePowerSum += power;
     });
