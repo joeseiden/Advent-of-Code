0cc58f729d190da8b96d65b7257a84a656df4527
Joe Seiden <joe.seiden214@gmail.com>
2024-09-19 12:07:31 -0700

    Refactor parseStacks to handle any input

--- 2022/day5.js
+++ 2022/day5.js
@@ -4,41 +4,25 @@ import _ from "lodash";
 const input = readFileSync('./inputs/day5.txt', 'utf8').split('\n\n');
 const exampleInput = readFileSync('./inputs/day5_example.txt', 'utf8').split('\n\n');
 
-const parseExampleStacks = (input) => {
-    // complete this later
-    return [
-        [],
-        'ZN'.split(''),
-        'MCD'.split(''),
-        'P'.split('')
-    ];
-};
+const parseStacks = (stacks) => {
+    const zippedStacks = _.zip(...stacks.split('\n').map(line => line.split('')).reverse());
 
-const parseStacks = (input) => {
-
-    return [
-        '',
-        'NCRTMZP',
-        'DNTSBZ',
-        'MHQRFCTG',
-        'GRZ',
-        'ZNRH',
-        'FHSWPZLD',
-        'WDZRCGM',
-        'SJFLHWZQ',
-        'SQPWN'
-    ].map(stack => stack.split(''));
+    let result = [[]];
+    zippedStacks.forEach((stack) => {
+        if(parseInt(stack[0])) {
+            result.push(stack.slice(1).filter(el => el != ' '))
+        }
+    })
+    return result;
 }
 
-const parseInstructions = (input) => {
-    const instructionsBlock = input[1];
+const parseInstructions = (instructionsBlock) => {
     return [...instructionsBlock.split('\n').map(instruction => [...instruction.matchAll(/\d+/g)].map(num => parseInt(num)))]
 };
 
 const part1 = (input) => {
-    // let stacks = parseExampleStacks();
-    let stacks = parseStacks(input);
-    const instructions = parseInstructions(input);
+    let stacks = parseStacks(input[0]);
+    const instructions = parseInstructions(input[1]);
 
 
     instructions.forEach((instruction) => {
@@ -52,6 +36,24 @@ const part1 = (input) => {
     return topCrates.join('');
 };
 
-// console.log(`Part 1 example solution: ${part1(exampleInput)}`);
+console.log(`Part 1 example solution: ${part1(exampleInput)}`);
 console.log(`Part 1 solution: ${part1(input)}`);
 
+const part2 = (input) => {
+    let stacks = parseStacks(input[0]);
+    const instructions = parseInstructions(input[1]);
+
+
+    instructions.forEach((instruction) => {
+        const [count, start, dest] = instruction;
+        const cratesToMove = stacks[start].slice(stacks[start].length - count);        
+        stacks[start] = stacks[start].slice(0, stacks[start].length - count);
+        stacks[dest].push(...cratesToMove);
+    });
+
+    const topCrates = stacks.map(stack => stack.at(-1));
+    return topCrates.join('');
+};
+
+console.log(`Part 2 example solution: ${part2(exampleInput)}`);
+console.log(`Part 2 solution: ${part2(input)}`);
