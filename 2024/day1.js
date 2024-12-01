c690bb53a56dfe89c8cd1c1beef4a8559405b646
Joe Seiden <joe.seiden214@gmail.com>
2024-12-01 12:49:45 -0800

    Solve AoC 2024 Day 1 and update gitignores

---
+++ 2024/day1.js
@@ -0,0 +1,38 @@
+import { readFileSync } from "fs";
+import _ from 'lodash';
+
+const exampleInput = readFileSync('./inputs/day1_example.txt', 'utf8');
+const input = readFileSync('./inputs/day1.txt', 'utf8');
+
+const parseInput = (input) => {
+    return input.split('\n').map(pair => pair.split('   ').map(num => parseInt(num)));
+}
+
+const part1 = (input) => {
+    const list = parseInput(input);
+    const zippedList = _.zip(...list);
+    const sortedZipList = zippedList.map(subArr => subArr.sort());
+    const sortedPairs = _.zip(...sortedZipList);
+    const distances = sortedPairs.map(pair => Math.abs(pair[0] - pair[1]));
+    const totalDistance = distances.reduce((a, b) => a + b);
+    
+    return totalDistance;
+}
+
+
+console.log(`Part 1 example solution: ${part1(exampleInput)}`);
+console.log(`Part 1 solution: ${part1(input)}`);
+
+const part2 = (input) => {
+    const list = parseInput(input);
+    const [leftList, rightList] = _.zip(...list);
+    const rightListOccurences = _.countBy(rightList);
+    const similarityScore = leftList.map(num => num * rightListOccurences[num] || 0).reduce((a ,b) => a + b);
+    
+    return similarityScore;
+}
+
+console.log(`Part 2 example solution: ${part2(exampleInput)}`);
+console.log(`Part 2 solution: ${part2(input)}`);
+
+
