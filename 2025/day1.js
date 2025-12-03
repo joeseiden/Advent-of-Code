0c91b98dddf096bf79de6f61033a667b3851c65e
Joe Seiden <jseiden@atlassian.com>
2025-12-03 14:46:13 -0800

    AOC 2025 Day 1: Solve Part 2

--- 2025/day1.js
+++ 2025/day1.js
@@ -15,7 +15,7 @@ const part1 = (input) => {
     let currentPos = START_POS
     let zeroCount = 0
     parsedInput.forEach(command => {
-        currentPos = executeCommand(currentPos, command)
+        currentPos = executeCommand(currentPos, command[0], command[1] % MAX_VALUE).newPos
         if(currentPos === 0) {
             zeroCount++
         }
@@ -24,19 +24,24 @@ const part1 = (input) => {
     return zeroCount;
 }
 
-const executeCommand = (currentPos, command) => {
-    const direction = command[0]
-    const distance = command[1] % MAX_VALUE
+const executeCommand = (currentPos, direction, distance) => {
+    let didPassZero = false
     let newPos
     if (direction === 'R') {
+        if (currentPos + distance > MAX_VALUE) {
+            didPassZero = true
+        }
         newPos = (currentPos + distance) % MAX_VALUE
     } else if (direction === 'L') {
         newPos = (currentPos - distance)
         if(newPos < 0) {
+            if(currentPos !== 0) {
+                didPassZero = true
+            }
             newPos = MAX_VALUE + newPos
         }
     }
-    return newPos
+    return { newPos, didPassZero }
 }
 
 console.log(`Part 1 example solution: ${part1(exampleInput)}`);
@@ -44,8 +49,22 @@ console.log(`Part 1 solution: ${part1(input)}`);
 
 const part2 = (input) => {
     const parsedInput = parseInput(input);
-    return true;
+    let currentPos = START_POS
+    let zeroCount = 0
+    parsedInput.forEach(command => {
+        // console.log(currentPos, command, zeroCount);
+        
+        const rotationCount = Math.floor(command[1] / MAX_VALUE)
+        zeroCount = zeroCount + rotationCount
+        const { newPos, didPassZero } = executeCommand(currentPos, command[0], command[1] % MAX_VALUE)
+        if(newPos === 0 || didPassZero) {
+            zeroCount++
+        }
+        currentPos = newPos
+    });
+
+    return zeroCount;
 }
 
-// console.log(`Part 2 example solution: ${part2(exampleInput)}`);
-// console.log(`Part 2 solution: ${part2(input)}`);
+console.log(`Part 2 example solution: ${part2(exampleInput)}`);
+console.log(`Part 2 solution: ${part2(input)}`);
